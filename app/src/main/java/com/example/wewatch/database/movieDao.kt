52eee2b2170824518bea71340c50c45e17546f38
kosenkova-j <kojulev@gmail.com>
2025-05-28 23:43:16 +0700

    создание дб

---
+++ app/src/main/java/com/example/wewatch/database/movieDao.kt
@@ -0,0 +1,19 @@
+package com.example.wewatch.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM movies")
+    suspend fun getAllMovies(): List<Movie>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(movie: Movie)
+
+    @Delete
+    suspend fun delete(movie: Movie)
+}
